845d10d4aaf836465bb99aaa135f62e0cb4a3280
114050_RODI_CARO_Nicolas <114050@tecnicatura.frc.utn.edu.ar>
2024-08-03 11:49:00 -0300

    commit zero

--- src/main/java/com/example/rentacar/repositories/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rentacar.repositories;
-
-import com.example.rentacar.domain.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
